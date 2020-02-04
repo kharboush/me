ba86b69e8f525b568849de3a57224e6c926e8de4
parichkova <tyhomiraparichkova@gmail.com>
2020-02-04 21:29:43 +0200

    refactor modal, set action buttons to the right

--- projects/giphy/scripts/localStorage.js
+++ projects/giphy/scripts/localStorage.js
@@ -1,23 +1,23 @@
 export const store = (id, key) => {
   const stored = localStorage.getItem(key);
   const storedId = id;
-  if (typeof Storage !== 'undefined') {
+
+  if (Storage !== undefined) {
     if (stored === null) {
       localStorage.setItem(key, `${storedId}`);
     } else {
       localStorage.setItem(key, [stored, `${storedId}`]);
     }
   } else {
-    alert(
-      "Sorry, your browser does not support Web Storage, so we can't remember that"
-    );
+    alert('Sorry, your browser does not support Web Storage, so we can\'t remember that');
   }
 };
 
 export const del = (id, key) => {
-  let stored = localStorage.getItem(key);
   const storedId = id;
-  if (typeof Storage !== 'undefined') {
+  let stored = localStorage.getItem(key);
+  
+  if (Storage !== undefined) {
     if (stored.includes(`${storedId},`)) {
       stored = stored.replace(`${storedId},`, '');
     } else {
@@ -25,10 +25,6 @@ export const del = (id, key) => {
     }
     localStorage.setItem(key, stored);
   } else {
-    alert(
-      "Sorry, your browser does not support Web Storage, so we can't remember that"
-    );
+    alert('Sorry, your browser does not support Web Storage, so we can\'t remember that');
   }
 };
-
-// const id = $(ev.target).attr('id');
